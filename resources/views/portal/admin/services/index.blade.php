5db2465ea907a0dbd7da7b4b97af419e9c330f52
Your Name <107371667+mohammedalsersawi@users.noreply.github.com>
2023-04-02 20:47:06 +0300

    fixxxxxxxx

--- resources/views/portal/admin/services/index.blade.php
+++ resources/views/portal/admin/services/index.blade.php
@@ -118,6 +118,8 @@
                             </div>
                         @endforeach
 
+
+
                         {{-- <div class="row">
                             <div class="col-12">
                                 <div class="form-group">
